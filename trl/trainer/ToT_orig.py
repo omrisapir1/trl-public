1e42785b918d86e24263476b431df8be8b929f29
Omri <omri.sapir@workday.com>
2025-03-27 14:05:04 +0200

    all

--- trl/trainer/ToT_orig.py
+++ trl/trainer/ToT_orig.py
@@ -13,15 +13,25 @@ TOP_P = 1.0
 TOP_K = 50
 REPETITION_PENALTY = 1.0
 MODEL = 'omrisap/Qwen2.5-1.5B_30K_COT_SFT'
-THINK_TAG_START = '<think></think>'
 THINK_END_TOKEN = '</think>'
+THINK_START_TOKEN = '<think>'
+THINK_BOTH_TOKEN = '<think></think>'
+
 ANSWER_END_TOKEN = '</answer>'
+ANSWER_START_TOKEN = '<answer>'
 END_OF_TEXT_ID_TOKEN = 151643
 
 UNIFIED_MAX_TOKENS = 512
+MAX_FIRST_ANS_TOKENS = 2548
+
+N_TOTAL_SPLITS = 2
+LAST_SPLIT = 4
+CORRECT_STRUCTURE_REWARD = 0.1
+CORRECT_FLOAT_REWARD = 0.1
+FIRST_SPLIT_COUNT = 4
 
 class TreeOfThoughts:
-    def __init__(self, llm, max_split_depth=24, max_depth=25):
+    def __init__(self, llm, max_split_depth=34, max_depth=9):
         self.llm = llm
         self.max_depth = max_depth
         self.max_split_depth = max_split_depth
@@ -33,7 +43,7 @@ class TreeOfThoughts:
             top_k=TOP_K,
             repetition_penalty=REPETITION_PENALTY,
             skip_special_tokens=False,
-            stop=["</think>", '</answer>'],
+            stop=["</think>", '</answer>','<answer>'],
             n=1  # Generate one continuation per prompt
         )
         self.last_sampling_params = SamplingParams(
@@ -46,28 +56,45 @@ class TreeOfThoughts:
             stop=['</answer>'],
             n=1  # Generate one continuation per prompt
         )
-        self.no_more_splits_sampling_params = SamplingParams(
+        # self.no_more_splits_sampling_params = SamplingParams(
+        #     temperature=TEMPERATURE,
+        #     max_tokens=UNIFIED_MAX_TOKENS,
+        #     # (self.max_depth - self.max_split_depth) * MAX_THINK_TOKENS + MAX_END_TOKENS,
+        #     top_p=TOP_P,
+        #     top_k=TOP_K,
+        #     repetition_penalty=REPETITION_PENALTY,
+        #     skip_special_tokens=False,
+        #     stop=['</answer>'],
+        #     n=1  # Generate one continuation per prompt
+        # )
+        self.first_full_ans = SamplingParams(
             temperature=TEMPERATURE,
-            max_tokens=UNIFIED_MAX_TOKENS,#(self.max_depth - self.max_split_depth) * MAX_THINK_TOKENS + MAX_END_TOKENS,
+            max_tokens=MAX_FIRST_ANS_TOKENS,
+            # (self.max_depth - self.max_split_depth) * MAX_THINK_TOKENS + MAX_END_TOKENS,
             top_p=TOP_P,
             top_k=TOP_K,
             repetition_penalty=REPETITION_PENALTY,
             skip_special_tokens=False,
-            stop=['</answer>'],
+            stop=['<answer>'],
             n=1  # Generate one continuation per prompt
         )
 
     def is_correct_solution(self,answer, numerical_label):
         """Extract the last numerical answer enclosed in \boxed{}"""
+        total_reward = 0
         matches = re.findall(r'\\boxed\{([^}]*)\}', answer)
         if matches:
             extracted_answer = re.sub(r'[^0-9.-]', '', matches[-1]).strip()
             if extracted_answer.replace('.', '', 1).replace('-','',1).isdigit():
+
                 try:
-                    return int(float(extracted_answer) == float(numerical_label))
+                    extracted_answer = float(extracted_answer)
+                    total_reward += CORRECT_FLOAT_REWARD
+                    total_reward += int(float(extracted_answer) == float(numerical_label))
+                    return total_reward
                 except ValueError:
-                    return 0
-        return 0
+                    return total_reward
+        return total_reward
 
     def preprocess_problem(self, problem):
         # Ensure the contents are strings
@@ -90,14 +117,18 @@ class TreeOfThoughts:
             return 0
         elif node.get('last_chance'):
             return 1
-        if node['depth'] == 0:
-            return 2
-        elif node['depth'] == 1:
-            return 2
-        elif node['depth'] == 2:
-            return 3
-        elif node['depth'] == 3:
-            return 4
+        elif node.get('next_split'):
+            return node['next_split']
+        # if node['depth'] == 0:
+        #     return 4
+        # elif node['depth'] == 1:
+        #     return 2
+        # elif node['depth'] == 2:
+        #     return 2
+        # elif node['depth'] == 3:
+        #     return 2
+        # elif node['depth'] == 4:
+        #     return 2
         return 1
 
 
@@ -106,16 +137,62 @@ class TreeOfThoughts:
         # Tree structure: list of dictionaries with 'text' and 'parent_idx'
         problem_as_chat_prompt = self.preprocess_problem(problem)
         tree = [{
-            'text': problem_as_chat_prompt + THINK_TAG_START,
+            'text': problem_as_chat_prompt + THINK_BOTH_TOKEN,
             'prompt': '',
             'parent_idx': None,
             'depth': 0,
-            'split': None,
+            'split': -1,
             'last_chance':False
         }]
 
-        current_depth = 0
         final_nodes = []
+
+        first_full_outputs = self.llm.generate([tree[0]['text']] * FIRST_SPLIT_COUNT, self.first_full_ans)
+        any_valid_end = False
+        for first_full_output in first_full_outputs:
+            first_full_completion = first_full_output.outputs[0]
+            full_ans = first_full_completion.text
+            node = {'prompt': tree[0]['text'],
+                    'parent_idx': 0,
+                    'last_chance': False,
+                    'depth': 1,
+                    'split': FIRST_SPLIT_COUNT,
+                    'text': (full_ans + first_full_completion.stop_reason) if type(first_full_completion.stop_reason)==str else '',
+                    'predict_answer': True,
+                    'prompt_ids': first_full_output.prompt_token_ids,
+                    'completion_ids': first_full_completion.token_ids,
+                    'next_split':LAST_SPLIT,
+                    }
+
+            if ANSWER_END_TOKEN in full_ans or first_full_completion.finish_reason == 'length' or first_full_completion.stop_reason == END_OF_TEXT_ID_TOKEN or first_full_completion.stop_reason is None or first_full_completion.stop_reason != ANSWER_START_TOKEN:
+                node['reward'] = 0
+                node['to_stop'] = True
+                final_nodes.append((0, node['reward']))
+                print('1 Down')
+
+            else:
+                any_valid_end = True
+            tree.append(node)
+            #
+            #
+            # thoughts_count = full_ans.count(THINK_END_TOKEN) + 1
+            #
+            # if thoughts_count > N_TOTAL_SPLITS:
+            #     start_index = kth_occurrence_from_end(full_ans, THINK_BOTH_TOKEN, N_TOTAL_SPLITS) + len(THINK_BOTH_TOKEN)
+            #     tree[0]['text'] += full_ans[:start_index]
+
+
+
+
+        # completions = [output.outputs[0] for output in first_full_output]
+        # all_prompts_token_ids = [output.prompt_token_ids for output in outputs]
+        if not any_valid_end:
+            print('Didnt found any valid end of text')
+            return tree, [], []
+        current_depth = 1
+
+        logs = []
+        counter_max_depth, counter_not_max_depth = 0, 0
         while current_depth <= self.max_depth:
             # Get unsplit nodes at current depth
             candidates, idxs = zip(*[(n, i) for i, n in enumerate(tree) if n['depth'] == current_depth])
@@ -123,14 +200,20 @@ class TreeOfThoughts:
             # Collect nodes to split and their split counts
             split_nodes = []
             for node, idx in zip(candidates, idxs):
+
+
                 if current_depth == self.max_depth:
-                    final_nodes.append((node['parent_idx'], 0))
+                    counter_max_depth += 1
+                    logs.append(f'ADDed in MAX depth {counter_max_depth} and idx is {idx}')
+                    final_nodes.append((node['parent_idx'], node['reward']))
                     continue
                 split_count = self.decide_split(node)
                 node['split'] = split_count
+
                 if split_count:
                     split_nodes.append((node, split_count, idx))
                 else:
+                    logs.append(f'ADDed not in max depth {counter_not_max_depth} and idx is {idx}')
                     final_nodes.append((node['parent_idx'], node['reward']))
 
             if not split_nodes:
@@ -144,9 +227,7 @@ class TreeOfThoughts:
                 split_mapping.append((idx, split_count))
 
             # Generate all continuations in single batch
-            if current_depth == self.max_split_depth:
-                outputs = self.llm.generate(batch_prompts, self.no_more_splits_sampling_params)
-            elif current_depth == (self.max_depth-1):
+            if current_depth == (self.max_depth-1):
                 outputs = self.llm.generate(batch_prompts, self.last_sampling_params)
             else:
                 outputs = self.llm.generate(batch_prompts, self.sampling_params)
@@ -163,37 +244,51 @@ class TreeOfThoughts:
                 prompts_token_ids = all_prompts_token_ids[comp_idx:comp_idx + split_count]
                 comp_idx += split_count
                 if split_count == 1:
+
                     children_completion = children_completions[0]
                     text = children_completion.text
+
                     if children_completion.finish_reason == 'length' or children_completion.stop_reason == END_OF_TEXT_ID_TOKEN or children_completion.stop_reason is None:
                         if parent.get('last_chance') or children_completion.stop_reason == END_OF_TEXT_ID_TOKEN or children_completion.stop_reason is None:
                             parent['to_stop'] = True
                             parent['reward'] = 0
                         else:
                             parent['last_chance'] = True
+                        stop_token = children_completion.stop_reason
+                        if stop_token and not parent.get('to_stop'):
+                            print(stop_token)
+                            raise
                         text = ' ' + text
                     else:
-
+                        parent['last_chance'] = False
                         stop_token = children_completion.stop_reason
                         text += stop_token
-
-                        if stop_token == ANSWER_END_TOKEN:
-                            parent['reward'] = self.is_correct_solution(text, numerical_label)
+                        if parent.get('predict_answer') and any(t in text for t in [THINK_END_TOKEN, THINK_START_TOKEN, ANSWER_START_TOKEN]):
+                            parent['reward'] = 0
                             parent['to_stop'] = True
-                    parent['last_chance'] = False
+                        elif stop_token == ANSWER_END_TOKEN:
+                            if ANSWER_START_TOKEN in text or ANSWER_START_TOKEN in parent['prompt'] or ANSWER_START_TOKEN in parent['text']:
+                                parent['reward'] = self.is_correct_solution(text, numerical_label) + CORRECT_STRUCTURE_REWARD
+                            else:
+                                parent['reward'] = 0
+                            parent['to_stop'] = True
+
+                        elif stop_token == ANSWER_START_TOKEN:
+                            parent['next_split'] = LAST_SPLIT
+                            parent['predict_answer'] = True
+
 
                     parent['completion_ids'] += children_completion.token_ids
 
 
                     parent['text'] += text
-                    parent['prompt'] += text
                     parent['depth'] += 1
                     continue
 
 
                 for children_completion, prompt_token_ids in zip(children_completions, prompts_token_ids):
+
                     node = {
-                        'orig_prompt':parent['prompt'] + parent['text'],
                         'prompt': parent['prompt'] + parent['text'],
                         'parent_idx': parent_idx,
                         'depth': current_depth + 1,
@@ -203,14 +298,26 @@ class TreeOfThoughts:
                     text = children_completion.text
                     if children_completion.finish_reason == 'length':
                         node['last_chance'] = True
+                        node['predict_answer'] = parent.get('predict_answer')
                         text = ' ' + text
                     elif children_completion.stop_reason == THINK_END_TOKEN:
                         text += THINK_END_TOKEN
                     elif children_completion.stop_reason == ANSWER_END_TOKEN:
                         text += ANSWER_END_TOKEN
-                        node['reward'] = self.is_correct_solution(text, numerical_label)
+                        if ANSWER_START_TOKEN in text or ANSWER_START_TOKEN in node['prompt']:
+                            node['reward'] = self.is_correct_solution(text, numerical_label) + CORRECT_STRUCTURE_REWARD
+                        else:
+                            node['reward'] = 0
                         node['to_stop'] = True
-                    elif children_completion.stop_reason == END_OF_TEXT_ID_TOKEN:
+                    elif children_completion.stop_reason == END_OF_TEXT_ID_TOKEN or children_completion.stop_reason is None:
+                        node['reward'] = 0
+                        node['to_stop'] = True
+                        node['predict_answer'] = parent.get('predict_answer')
+                    elif children_completion.stop_reason == ANSWER_START_TOKEN:
+                        text += ANSWER_START_TOKEN
+                        node['next_split'] = LAST_SPLIT
+                        node['predict_answer'] = True
+                    if parent.get('predict_answer') and any(t in text for t in [THINK_END_TOKEN, THINK_START_TOKEN, ANSWER_START_TOKEN]):
                         node['reward'] = 0
                         node['to_stop'] = True
                     node['text'] = text
@@ -218,7 +325,7 @@ class TreeOfThoughts:
 
             current_depth += 1
 
-        return tree, final_nodes
+        return tree, final_nodes, logs
 
     def propogate_reward(self, node_idx, reward, tree):
         node = tree[node_idx]
@@ -231,18 +338,45 @@ class TreeOfThoughts:
 
 
 
-    def evaluate_tree(self, tree, final_nodes):
+    def evaluate_tree(self, tree, final_nodes, logs):
         for node in final_nodes:
             self.propogate_reward(node[0], node[1], tree)
+        try:
+            final_nodes_rewards = [n['reward'] for n in tree if n.get('reward', -111) >= 0 and not n.get('rewards')]
+            depth_1_rewards = []
+            for n in tree:
+                if n['depth'] == 1:
+                    rewards = n.get('rewards') or [n['reward']]
+                    depth_1_rewards.extend(rewards)
+            assert sorted(depth_1_rewards) == sorted(final_nodes_rewards)
+        except:
+            print(logs)
+            print(tree)
+            raise
+
         for n in tree:
-            rewards = n.get('rewards',[1])
-            std = np.std(rewards)
-            if std:
-                n['reward'] = np.mean(rewards)
+            reward = n.get('reward')
+            if reward is not None:
+                n['reward'] = reward
             else:
-                n['dont_calc_loss'] = True
+                n['reward'] = np.mean(n['rewards'])
+
+
         if random.random() < 1:
             json.dump(tree,open(f'/workspace/Data_in_training/{time.time()}','w'))
+
         else:
             json.dump([{'rewards':t.get('rewards'), 'reward':t.get('reward'), 'parent_idx':t.get('parent_idx')} for t in tree], open(f'/workspace/Data_in_training/{time.time()}', 'w'))
 
+
+def kth_occurrence_from_end(s, sub, k):
+    """
+    Returns the index of the k'th occurrence of `sub` in `s` from the end.
+    If the substring doesn't occur k times, returns -1.
+    """
+    pos = len(s)
+    for i in range(k):
+        pos = s.rfind(sub, 0, pos)
+        if pos == -1:
+            return -1
+    return pos
