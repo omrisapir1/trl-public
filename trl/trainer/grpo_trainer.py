5ff0634e3d0f80f5a75c1f4105a66ed793e623ed
Omri <omri.sapir@workday.com>
2025-03-30 21:26:09 +0300

    all

--- trl/trainer/grpo_trainer.py
+++ trl/trainer/grpo_trainer.py
@@ -515,7 +515,7 @@ class GRPOTrainer(Trainer):
                         model=model.name_or_path,
                         # tensor_parallel_size=2,
                         device=vllm_device,
-                        gpu_memory_utilization=0.6,
+                        gpu_memory_utilization=0.7,
                         # dtype=torch.bfloat16,
                         max_num_seqs=64,
 
@@ -911,7 +911,7 @@ class GRPOTrainer(Trainer):
         except:
             torch.cuda.empty_cache()
             per_token_logps = None
-            
+
 
         if per_token_logps is None:
             return
