a8783e0ed05375fce3c86d83b0addba28ff6d796
Omri <omri.sapir@workday.com>
2025-04-04 17:10:23 +0300

    all

--- trl/trainer/grpo_trainer.py
+++ trl/trainer/grpo_trainer.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import gc
 import os
 import textwrap
 import warnings
@@ -482,7 +482,7 @@ class GRPOTrainer(Trainer):
                                 model=model.name_or_path,
                                 # tensor_parallel_size=2,
                                 # device='cuda:1',
-                                gpu_memory_utilization=0.4,
+                                gpu_memory_utilization=0.2,
                                 dtype=torch.bfloat16,
                                 max_num_seqs=16,
 
@@ -731,6 +731,7 @@ class GRPOTrainer(Trainer):
 
         losses = []
         # Compute loss in micro-batches for each group.
+        gc.collect()
         for i, group in enumerate(inputs):
             with self.compute_loss_context_manager():
                 # Compute a loss per group.
