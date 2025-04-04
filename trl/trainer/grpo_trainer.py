89155d6145824ce1586804103a9227b911ed6442
Omri <omri.sapir@workday.com>
2025-04-04 14:08:48 +0300

    all

--- trl/trainer/grpo_trainer.py
+++ trl/trainer/grpo_trainer.py
@@ -1291,7 +1291,7 @@ class GRPOTrainer(Trainer):
 
         # Compute the KL divergence between the model and the reference model
         if self.beta != 0.0:
-            ref_per_token_logps = inputs["ref_per_token_logps"]
+            ref_per_token_logps = inputs["ref_per_token_logps"].to(self.model.device)
             per_token_kl = (
                 torch.exp(ref_per_token_logps - per_token_logps) - (ref_per_token_logps - per_token_logps) - 1
             )
