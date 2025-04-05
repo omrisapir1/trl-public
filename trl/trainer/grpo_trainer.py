2568dd03b4a6dd1394b3fe8530c2ce0613174c29
Omri <omri.sapir@workday.com>
2025-04-05 15:47:37 +0300

    all

--- trl/trainer/grpo_trainer.py
+++ trl/trainer/grpo_trainer.py
@@ -488,7 +488,7 @@ class GRPOTrainer(Trainer):
                                 model=model.name_or_path,
                                 # tensor_parallel_size=2,
                                 device='cuda:1',
-                                gpu_memory_utilization=0.2,
+                                gpu_memory_utilization=0.6,
                                 dtype=torch.bfloat16,
                                 max_num_seqs=16,
 
@@ -903,7 +903,7 @@ class GRPOTrainer(Trainer):
                 per_token_logps = self._get_per_token_logps(model, input_ids.to(model.device),
                                                             attention_mask.to(model.device), logits_to_keep)
         except:
-            
+
             for inpt in input_ids:
                 print(self.tokenizer.decode(inpt))
             print(input_ids.shape)
