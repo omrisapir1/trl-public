84d62d182f057c650f8dbcfc20d354f01f8e8603
Omri <omri.sapir@workday.com>
2025-04-05 16:09:09 +0300

    all

--- trl/trainer/grpo_trainer.py
+++ trl/trainer/grpo_trainer.py
@@ -934,6 +934,7 @@ class GRPOTrainer(Trainer):
         if self.beta != 0.0:
             per_token_loss = per_token_loss + self.beta * per_token_kl
         print(per_token_loss1.shape)
+        print(per_token_loss1)
         loss = (per_token_loss * completion_mask.to(model.device)).sum() / MAX_TOKENS_TO_CALC_LOSS
         del per_token_loss, per_token_loss2, per_token_loss1, completion_mask, advantages, coef_2, coef_1, per_token_kl, ref_per_token_logps, per_token_logps
         torch.cuda.empty_cache()
