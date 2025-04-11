1c15b017e36b193b184df9e6d4a65641fff528b9
Omri <omri.sapir@workday.com>
2025-04-11 15:09:47 +0300

    all

--- trl/trainer/ToT.py
+++ trl/trainer/ToT.py
@@ -388,7 +388,7 @@ class TreeOfThoughts:
 
 
         current_depth = 1
-        max_depth = self.max_split_depth + 2
+        self.max_depth = self.max_split_depth + 2
         while current_depth < self.max_depth:
             active_nodes = [node for node in self.get_all_nodes(root) if node.depth == current_depth and not node.is_terminal()]
             if not active_nodes:
