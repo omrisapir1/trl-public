4e9ad492776b182f957599b0bb89648101a88b61
Omri <omri.sapir@workday.com>
2025-04-22 20:28:21 +0300

    all

--- trl/trainer/grpo_trainer.py
+++ trl/trainer/grpo_trainer.py
@@ -493,7 +493,7 @@ class GRPOTrainer(Trainer):
                                 model=model.name_or_path,
                                 # tensor_parallel_size=2,
                                 # device='cuda:1',
-                                gpu_memory_utilization=0.3,
+                                gpu_memory_utilization=0.55,
                                 dtype=torch.bfloat16,
                                 max_num_seqs=64,
 
