3baf307e4540ac2d1ce9725e77fbe6013c312d6b
Omri <omri.sapir@workday.com>
2025-05-09 14:24:17 +0300

    all

--- trl/trainer/grpo_trainer.py
+++ trl/trainer/grpo_trainer.py
@@ -792,7 +792,7 @@ class GRPOTrainer(Trainer):
         return loss.detach()
 
     @profiling_decorator
-    async def _prepare_inputs(self, inputs: dict[str, Union[torch.Tensor, Any]]) -> dict[str, Union[torch.Tensor, Any]]:
+    def _prepare_inputs(self, inputs: dict[str, Union[torch.Tensor, Any]]) -> dict[str, Union[torch.Tensor, Any]]:
         mode = "eval" if self.control.should_evaluate else "train"
         problem = [x["problem"] for x in inputs][0]
         final_answer = [x["final_answer"] for x in inputs][0]
@@ -800,7 +800,7 @@ class GRPOTrainer(Trainer):
             buffer_index = self._step % self.args.gradient_accumulation_steps
             buffered_inputs = self._buffered_inputs[buffer_index]
             if self.state.global_step % self.num_iterations == 0 or buffered_inputs is None:
-                tree_root = await self.tree_of_thought.expand_tree(problem, final_answer)
+                tree_root = self.tree_of_thought.expand_tree(problem, final_answer)
                 inputs = self._convert_tree_to_training_inputs(tree_root)
                 self._buffered_inputs[buffer_index] = inputs
             else:
@@ -808,7 +808,7 @@ class GRPOTrainer(Trainer):
             self._step += 1
         else:
             # In evaluation, we don't reuse completions across multiple updates, so we don't need to buffer inputs.
-            tree_root = await self.tree_of_thought.expand_tree(problem, final_answer)
+            tree_root = self.tree_of_thought.expand_tree(problem, final_answer)
             inputs = self._convert_tree_to_training_inputs(tree_root)
         return inputs
 
