6dfa42fd06dde9c9e6ac5cb9a1094537043f73b5
Omri <omri.sapir@workday.com>
2025-05-09 14:35:23 +0300

    all

--- trl/trainer/ToE.py
+++ trl/trainer/ToE.py
@@ -130,8 +130,13 @@ class TreeOfThoughtsEntropyVLLM:
         if take_one_from_prompt:
             init_addition_tokens.append(node.prompt_ids[-1])
             node.prompt_ids = node.prompt_ids[:-1]
+        try:
 
-        node.completion_ids = init_addition_tokens + output.token_ids
+            node.completion_ids = init_addition_tokens + output.token_ids
+        except:
+            print(output.token_ids)
+            print(init_addition_tokens)
+            raise 
         if remove_last_token:
             last_token_id = node.completion_ids[-1]
             node.completion_ids = node.completion_ids[:-1]
