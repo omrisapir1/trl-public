0b7ba1f02cb522b68802906e89b8ba8bfd33e760
Omri <omri.sapir@workday.com>
2025-05-22 11:12:11 +0300

    all

--- trl/trainer/grpo_trainer.py
+++ trl/trainer/grpo_trainer.py
@@ -784,47 +784,7 @@ class GRPOTrainer(Trainer):
             # This must be done after loading weights to ensure they correspond to the merged state.
             if is_peft_model(unwrapped_model):
                 unwrapped_model.unmerge_adapter()
-    #
-    # @profiling_decorator
-    # def _move_model_to_vllm(self):
-    #     # For DeepSpeed ZeRO-3, we need to gather all parameters before operations
-    #     deepspeed_plugin = self.accelerator.state.deepspeed_plugin
-    #     zero_stage_3 = deepspeed_plugin is not None and deepspeed_plugin.zero_stage == 3
-    #     gather_if_zero3 = deepspeed.zero.GatheredParameters if zero_stage_3 else nullcontext
-    #
-    #     if is_peft_model(self.model):
-    #         # With PEFT and DeepSpeed ZeRO Stage 3, we must gather the full model at once before merging, as merging
-    #         # adapters in a sharded manner is not supported.
-    #         with gather_if_zero3(list(self.model.parameters())):
-    #             self.model.merge_adapter()
-    #
-    #             # Update vLLM weights while parameters are gathered
-    #             for name, param in self.model.named_parameters():
-    #                 # When using PEFT, we need to recover the original parameter name and discard some parameters
-    #                 name = name.removeprefix("base_model.model.").replace(".base_layer", "")
-    #                 if self.model.prefix in name:
-    #                     continue
-    #                 # When module to save, remove its prefix and discard the original module
-    #                 if "original_module" in name:
-    #                     continue
-    #                 name = name.replace("modules_to_save.default.", "")
-    #
-    #                 if self.accelerator.is_main_process:
-    #                     self.vllm_client.update_named_param(name, param.data)
-    #
-    #             # Unmerge adapters while parameters are still gathered
-    #             self.model.unmerge_adapter()
-    #             # Parameters will automatically be repartitioned when exiting the context
-    #     else:
-    #         # For non-PEFT models, simply gather and update each parameter individually.
-    #         for name, param in self.model.named_parameters():
-    #             with gather_if_zero3([param]):
-    #                 if self.accelerator.is_main_process:
-    #                     self.vllm_client.update_named_param(name, param.data)
-    #
-    #     # Reset cache on main process
-    #     if self.accelerator.is_main_process:
-    #         self.vllm_client.reset_prefix_cache()
+
     def _maybe_log_save_evaluate(
         self, tr_loss, grad_norm, model, trial, epoch, ignore_keys_for_eval, start_time, learning_rate=None
     ):
@@ -854,7 +814,7 @@ class GRPOTrainer(Trainer):
 
 
         if self.control.should_save:
-
+            self.log_results_200()
             self._save_checkpoint(model, trial)
             self.control = self.callback_handler.on_save(self.args, self.state, self.control)
 
