d145d109f67e62d1e348197df8f89f99a7d862a4
Omri <omri.sapir@workday.com>
2025-05-23 22:48:21 +0300

    all

--- trl/trainer/grpo_trainer.py
+++ trl/trainer/grpo_trainer.py
@@ -529,12 +529,12 @@ class GRPOTrainer(Trainer):
                                 model=model.name_or_path,
                                 # tensor_parallel_size=2,
                                 # device='cuda:1',
-                                gpu_memory_utilization=0.4,
+                                gpu_memory_utilization=0.5,
                                 dtype=torch.bfloat16,
-                                max_num_seqs=256,
+                                max_num_seqs=1048,
                                 disable_log_stats=True,
 
-                                max_num_batched_tokens=64 * 2500,
+                                max_num_batched_tokens=64 * 5000,
                                 trust_remote_code=True,
 
                                 # tensor_parallel_size=2,
