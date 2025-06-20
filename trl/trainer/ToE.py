50b2f8bd63079fb9b0debf365283049d839ac179
Omri <omri.sapir@workday.com>
2025-06-20 09:45:29 +0300

    all

--- trl/trainer/ToE.py
+++ trl/trainer/ToE.py
@@ -16,7 +16,7 @@ from vllm.engine.async_llm_engine import AsyncLLMEngine
 
 
 MAX_STREAMS = 128
-TAU = 1.6#1#0.6  # threshold on EMA entropy
+TAU = 2.2#1#0.6  # threshold on EMA entropy
 # TAU = [1.1, 1.5 , 1.4 , 1.3, 0.9 ,0.9, 1.1]
 TEMP = 0.6
 TOP_P = 0.85
@@ -24,7 +24,7 @@ TOP_K = 20
 REP_PENALTY = 1.1
 LOGPROBS_K = 20
 MAX_TOKENS = 1300
-MIN_SPLIT_TOKENS = 300
+MIN_SPLIT_TOKENS = 120
 LAST_SPLIT_MIN_CHARS = 150
 
 
